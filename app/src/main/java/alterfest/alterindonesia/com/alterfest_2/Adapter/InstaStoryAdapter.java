c28d5415658f06302b91b253fd36c775ef36139b
febrian <febrianrz@gmail.com>
2018-12-12 14:13:57 +0700

    Menambahkan instastory

---
+++ app/src/main/java/alterfest/alterindonesia/com/alterfest_2/Adapter/InstaStoryAdapter.java
@@ -0,0 +1,58 @@
+package alterfest.alterindonesia.com.alterfest_2.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import alterfest.alterindonesia.com.alterfest_2.Data.InstaStoryData;
+import alterfest.alterindonesia.com.alterfest_2.R;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class InstaStoryAdapter extends RecyclerView.Adapter<InstaStoryAdapter.ViewHolder> {
+
+    private List<InstaStoryData> listData;
+    private Context context;
+
+    public InstaStoryAdapter(Context context, List<InstaStoryData> listData){
+        this.listData = listData;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_instastory, parent,false);
+        InstaStoryAdapter.ViewHolder vh = new InstaStoryAdapter.ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tvNama.setText(listData.get(position).getNama());
+        holder.imageView.setImageDrawable(context.getDrawable(listData.get(position).getGambar()));
+    }
+
+    @Override
+    public int getItemCount() {
+        if(listData == null) {
+            return 0;
+        }
+        return listData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView imageView;
+        TextView tvNama;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = (CircleImageView) itemView.findViewById(R.id.picture);
+            tvNama  = (TextView) itemView.findViewById(R.id.tvNama);
+        }
+    }
+}
--- app/src/main/java/alterfest/alterindonesia/com/alterfest_2/Fragments/BerandaFragment.java
+++ app/src/main/java/alterfest/alterindonesia/com/alterfest_2/Fragments/BerandaFragment.java
@@ -3,11 +3,18 @@ package alterfest.alterindonesia.com.alterfest_2.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import alterfest.alterindonesia.com.alterfest_2.Adapter.InstaStoryAdapter;
+import alterfest.alterindonesia.com.alterfest_2.Data.InstaStoryData;
 import alterfest.alterindonesia.com.alterfest_2.MainActivity;
 import alterfest.alterindonesia.com.alterfest_2.R;
 
@@ -18,6 +25,10 @@ public class BerandaFragment extends Fragment {
 
 
     View rootView;
+    RecyclerView rvInstaStory;
+    private LinearLayoutManager recylerLayoutManager;
+    private InstaStoryAdapter recylerAdapter;
+
 
     public BerandaFragment() {
         // Required empty public constructor
@@ -29,18 +40,37 @@ public class BerandaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_beranda, container, false);
+        rvInstaStory = (RecyclerView) rootView.findViewById(R.id.rvInstaStory);
         initToolbar();
+        initLayout();
         return rootView;
     }
 
     private void initToolbar() {
         Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
-//        toolbar.setNavigationIcon(R.drawable.ic_menu);
         ((MainActivity) getActivity()).setSupportActionBar(toolbar);
-//        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Toolbar");
-//        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initLayout(){
+        recylerLayoutManager    = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
+        rvInstaStory.setLayoutManager(recylerLayoutManager);
+        rvInstaStory.setHasFixedSize(true);
+        recylerAdapter = new InstaStoryAdapter(getContext(),getDataInstaStory());
+        rvInstaStory.setAdapter(recylerAdapter);
     }
 
 
-
+    private List<InstaStoryData> getDataInstaStory(){
+        List<InstaStoryData> list = new ArrayList<>();
+        list.add(new InstaStoryData("Umar",R.drawable.img1));
+        list.add(new InstaStoryData("Zaskia",R.drawable.img2));
+        list.add(new InstaStoryData("Aisyah",R.drawable.img3));
+        list.add(new InstaStoryData("Umar",R.drawable.img1));
+        list.add(new InstaStoryData("Zaskia",R.drawable.img2));
+        list.add(new InstaStoryData("Aisyah",R.drawable.img3));
+        list.add(new InstaStoryData("Umar",R.drawable.img1));
+        list.add(new InstaStoryData("Zaskia",R.drawable.img2));
+        list.add(new InstaStoryData("Aisyah",R.drawable.img3));
+        return list;
+    }
 }
